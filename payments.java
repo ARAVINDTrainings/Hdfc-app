da3f6858abdb829b1465dfad548fac6c4b85490a
ARAVIND Trainings <95344915+ARAVINDTrainings@users.noreply.github.com>
2026-08-07 18:29:55 +0530

    Add Main class with Hello World output

---
+++ payments.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    // The main method is the entry point where execution begins
+    public static void main(String[] args) {
+        
+        // Prints text to the terminal console
+        System.out.println("Hello, World!"); 
+    }
+}
